7c136a9b66380bdc4d63178dafa8f6010faa10d2
dmsazonov <dmsazonov@gmail.com>
2022-11-14 19:55:16 +0300

    Add springdoc-openapi-ui

--- src/main/java/com/simple/storage/controller/StorageController.java
+++ src/main/java/com/simple/storage/controller/StorageController.java
@@ -1,6 +1,7 @@
 package com.simple.storage.controller;
 
 import com.simple.storage.service.StorageService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +19,16 @@ public class StorageController {
 
     private final StorageService storageService;
 
-    @PostMapping("/store")
+    @PostMapping(value = "/store", consumes = {
+            "multipart/form-data"
+    })
+    @Operation(summary = "Store a file")
     public String storeFile(@RequestParam("file") MultipartFile file) throws IOException {
         return storageService.storeFile(file);
     }
 
     @GetMapping("/get/{id}")
+    @Operation(summary = "Get file by id")
     @ResponseBody
     public ResponseEntity<byte[]> getFile(@PathVariable String id) throws IOException {
         File result = storageService.getFile(id);
